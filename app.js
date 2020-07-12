3b4e977d78fe1a17f8aef66b9ae9fb9cd7bc701d
Jin Gyu Lee <ccjglingm07@gmail.com>
2020-07-12 18:36:06 -0500

    mixed

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const date = require(__dirname + '/date.js');
 
 const app = express();
 
-let items = ["Buy Food", "Cook Food", "Eat Food"];
-let workItems = [];
+const items = ["Buy Food", "Cook Food", "Eat Food"];
+const workItems = [];
 
 app.set('view engine', 'ejs'); //enable ejs in express
 app.use(bodyParser.urlencoded({extended: true}));
@@ -15,23 +16,15 @@ let address = ""
 // List for Today's To-do-List
 app.get("/", function(req, res) {
   address = req.url;
-  let today = new Date();
 
-  let options = {
-    weekday: 'long',
-    day: 'numeric',
-    month: 'long'
-  };
-
-  let day = today.toLocaleDateString("en-US", options);
+  const day = date.getDay();
 
   res.render("list", {listTitle: day, newListItems: items, theAddress: address}); // searches views folder and finds list.ejs
 });
 
-app.post("/", function(req, res){
-  let item = req.body.newItem;
+app.post("/", function(req, res) {
+  const item = req.body.newItem;
   items.push(item);
-
   res.redirect("/");
 });
 
@@ -41,12 +34,15 @@ app.get("/work", function(req, res) {
   res.render("list", {listTitle: "Work List", newListItems: workItems, theAddress: address});
 });
 
-app.post("/work", function(req, res){
-  let item = req.body.newItem;
+app.post("/work", function(req, res) {
+  const item = req.body.newItem;
   workItems.push(item);
   res.redirect("/work");
 });
 
+app.get("/about", function(req, res) {
+  res.render("about");
+});
 
 app.listen(3000, function() {
   console.log("Server started on port 3000.");
